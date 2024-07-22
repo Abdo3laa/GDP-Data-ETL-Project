1b57ccc5e6968e62f4eb8e9ad8095a75180cdb73
Abdo 3laa <113474869+Abdo3laa@users.noreply.github.com>
2024-07-22 18:26:09 +0300

    Add files via upload

---
+++ etl_project_wikipedia_gdp.py
@@ -0,0 +1,65 @@
+from bs4 import BeautifulSoup as soup
+import requests
+import pandas as pd
+import numpy as np
+import sqlite3
+from datetime import datetime 
+
+
+
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+table_attribs = ["Country", "GDP_USD_millions"]
+csv_path = "wikipedia_gdp.csv"
+
+def extract(url,table_attribs):
+    page = requests.get(url).text
+    data = soup(page,'html.parser')
+    df = pd.DataFrame(columns=table_attribs)
+    tables = data.find_all('tbody')
+    rows = tables[2].find_all('tr')
+    for row in rows:
+        col = row.find_all('td')
+        if len(col)!=0:
+            if col[0].find('a') is not None and '—' not in col[2]:
+                data_dict = {"Country": col[0].a.contents[0],
+                             "GDP_USD_millions": col[2].contents[0]}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
+
+    return df
+
+def transform(df):
+    GDP_list = df["GDP_USD_millions"].tolist()
+    GDP_list = [float("".join(x.split(','))) for x in GDP_list]
+    GDP_list = [np.round(x/1000,2) for x in GDP_list]
+    df["GDP_USD_millions"] = GDP_list
+    df=df.rename(columns = {"GDP_USD_millions":"GDP_USD_billions"})
+
+    return df
+
+def load_to_csv(df,csv_path):
+    df.to_csv(csv_path)
+
+def log_progress(message):
+    ''' This function logs the mentioned message at a given stage of the code execution to a log file. Function returns nothing'''
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open("./etl_project_log.txt","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n')
+
+log_progress('Preliminaries complete. Initiating ETL process')
+
+df = extract(url, table_attribs)
+
+log_progress('Data extraction complete. Initiating Transformation process')
+
+df = transform(df)
+
+log_progress('Data transformation complete. Initiating loading process')
+
+load_to_csv(df, csv_path)
+
+log_progress('Data saved to CSV file')
+
+print("Done")
